bfa06d476d71fa9d8b12c1fb01f16d45ea035156
lucasliu08 <40473190+lucasliu08@users.noreply.github.com>
2019-06-11 15:39:10 -0300

    [ADD] Quantum input and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,7 @@ export class AppComponent {
     *     move -> final da fila_execucao
     *   incrementa tempo
     * */
-
+    console.log(this.quantum);
     let tempo = 0;
     let filaDeEspera = [...this.processControl];
     let filaDeExecucao: Process[] = [];
@@ -60,98 +60,6 @@ export class AppComponent {
     }
 
     console.log(matrizExecucao);
-
-    /*const numeroDeProcessos = this.processControl.length;
-    const chegada = [];
-    const execucao = [];
-    const espera = [];
-    const conclusao = [];
-    //let tempo = 0;
-    let sequencia = '';
-
-    // Cria listas de tempo de execução e chegada
-    for (let i = 0; i < numeroDeProcessos; i++) {
-      chegada[i] = this.processControl[i].arrivalTime;
-      execucao[i] = this.processControl[i].executionTime;
-    }
-
-    // Calcula os dados do Round Robin
-    while (true) {
-      let flag = true;
-      for (let i = 0; i < numeroDeProcessos; i++) {
-
-        if (chegada[i] <= tempo) {
-
-          if (chegada[i] <= this.quantum) {
-
-            if (execucao[i] > 0) {
-              flag = false;
-
-              if (execucao[i] > this.quantum) {
-                tempo = +this.quantum;
-                execucao[i] = -this.quantum;
-                chegada[i] = +this.quantum;
-                sequencia += '->' + this.processControl[i].name;
-              } else {
-                tempo = +execucao[i];
-                conclusao[i] = tempo - this.processControl[i].arrivalTime;
-                espera[i] = tempo - this.processControl[i].executionTime - this.processControl[i].arrivalTime;
-                execucao[i] = 0;
-                sequencia += '->' + this.processControl[i].name;
-              }
-            }
-          } else if (chegada[i] > this.quantum) {
-
-            for (let j = 0; j < numeroDeProcessos; j++) {
-
-              if (chegada[j] < chegada[i]) {
-
-                if (execucao[j] > 0) {
-                  flag = false;
-
-                  if (execucao[j] > this.quantum) {
-                    tempo = +this.quantum;
-                    execucao[j] = +this.quantum;
-                    chegada[j] = +this.quantum;
-                    sequencia += '->' + this.processControl[i].name;
-                  } else {
-                    tempo = +execucao[j];
-                    conclusao[j] = tempo - this.processControl[j].arrivalTime;
-                    espera[j] = tempo - this.processControl[j].executionTime - this.processControl[j].arrivalTime;
-                    execucao[j] = 0;
-                    sequencia += '->' + this.processControl[j].name;
-                  }
-                }
-              }
-            }
-
-            if (execucao[i] > 0) {
-              flag = false;
-
-              if (execucao[i] > this.quantum) {
-                tempo = +this.quantum;
-                execucao[i] = -this.quantum;
-                chegada[i] = +this.quantum;
-                sequencia = +'->' + this.processControl[i].name;
-              } else {
-                tempo = +execucao[i];
-                conclusao[i] = tempo - this.processControl[i].arrivalTime;
-                espera[i] = tempo - this.processControl[i].arrivalTime - this.processControl[i].executionTime;
-                execucao[i] = 0;
-                sequencia = +'->' + this.processControl[i].name;
-              }
-            }
-          }
-        } else if (chegada[i] > tempo) {
-          tempo++;
-          i--;
-        }
-      }
-      console.log(sequencia);
-      if (flag) {
-        break;
-      }
-    }*/
 
   }
 
